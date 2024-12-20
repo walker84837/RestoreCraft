2dd28a8654f11331fe43c8475951e35483b2b03d
winlogon <walker84837@gmail.com>
2024-12-20 11:28:41 +0100

    refactor: improve code quality

--- src/main/java/org/winlogon/restorecraft/HealCommand.java
+++ src/main/java/org/winlogon/restorecraft/HealCommand.java
@@ -5,7 +5,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffect;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
 
@@ -13,10 +12,11 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * Handles the /heal command.
+ * Handles the /heal command. 
  */
 public class HealCommand implements CommandExecutor {
     public boolean removeEffects;
+    public boolean showWhoHealed;
 
     private final List<String> errorMessages = List.of(
         "§cYou do not have permission to use this command.",
@@ -27,41 +27,38 @@ public class HealCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(errorMessages.get(1));
+            return true;
+        }
+
         if (!sender.hasPermission("heal.admin")) {
             sender.sendMessage(errorMessages.get(0));
             return true;
         }
 
-        if (!(sender instanceof Player) && args.length == 0) {
-            sender.sendMessage(errorMessages.get(1));
-            return true;
-        }
-
         Optional<Player> target;
 
-        if (args.length > 0) {
+        if (args.length <= 0) {
+            target = Optional.of((Player) sender);
+        } else {
             target = Optional.ofNullable(Bukkit.getPlayer(args[0]));
             if (target.isEmpty()) {
                 sender.sendMessage(errorMessages.get(2));
                 return true;
             }
-        } else {
-            if (!(sender instanceof Player)) {
-                sender.sendMessage(errorMessages.get(1));
-                return true;
-            }
-            target = Optional.of((Player) sender);
         }
 
-        target.ifPresentOrElse(player -> {
-            if (player.isOnline()) {
-                healPlayer(player);
-                sender.sendMessage(player.equals(sender) ? "§3You have been healed." : "§7"
-                    + player.getName() + "§3 has been healed.");
+        target.ifPresent(player -> {
+            healPlayer(player);
+            if (player.equals(sender)) {
+                sender.sendMessage("§3You have been healed.");
             } else {
-                sender.sendMessage(errorMessages.get(3));
+                sender.sendMessage("§7" + player.getName() + "§3 has been healed.");
+                player.sendMessage((showWhoHealed) ? "§3You have been healed by §7" 
+                    + sender.getName() + "§3." : "§3You have been healed.");
             }
-        }, () -> sender.sendMessage(errorMessages.get(2)));
+        });
 
         return true;
     }
@@ -72,18 +69,18 @@ public class HealCommand implements CommandExecutor {
      * @param player The player to heal.
 
      * @see HealCommand#healPlayer(Player)
-     * @returns void
+     * @return void
      */
     private void healPlayer(Player player) {
         AttributeInstance maxHealth = player.getAttribute(Attribute.MAX_HEALTH);
-        player.setHealth(maxHealth.getDefaultValue());
+        player.setHealth(maxHealth.getValue());
         player.setFoodLevel(20);
         player.setSaturation(20f);
 
         if (removeEffects) {
-            for (PotionEffect effect : player.getActivePotionEffects()) {
+            player.getActivePotionEffects().forEach(effect -> {
                 player.removePotionEffect(effect.getType());
-            }
+            });
         }
     }
 }
